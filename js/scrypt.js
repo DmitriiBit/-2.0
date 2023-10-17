ec90200c81315f3b719e9f16aff08c5ab1f29fa4
DmitriiK <dvhnjq@gmail.com>
2023-10-17 23:15:36 +0300

    17.10

--- js/scrypt.js
+++ js/scrypt.js
@@ -1,1 +1,29 @@
-new Swiper('.swiper-wrapper');
+(() => {
+    const refs = {
+      openModalBtn: document.querySelector("[data-modal-open]"),
+      closeModalBtn: document.querySelector("[data-modal-close]"),
+      modal: document.querySelector("[data-modal]"),
+    };
+  
+    refs.openModalBtn.addEventListener("click", toggleModal);
+    refs.closeModalBtn.addEventListener("click", toggleModal);
+  
+    function toggleModal() {
+      refs.modal.classList.toggle("is-hidden");
+    }
+})();
+  
+(() => {
+    const refs = {
+      openModalBtn: document.querySelector("[data-modal-op]"),
+      closeModalBtn: document.querySelector("[data-modal-clo]"),
+      modal: document.querySelector("[data-mo]"),
+    };
+  
+    refs.openModalBtn.addEventListener("click", toggleModal);
+    refs.closeModalBtn.addEventListener("click", toggleModal);
+  
+    function toggleModal() {
+      refs.modal.classList.toggle("is-hidden");
+    }
+  })();
