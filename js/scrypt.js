11acf4a5be3155e6e72575d1ab8a6b2af5ef5186
DmitriiK <dvhnjq@gmail.com>
2023-10-29 21:04:54 +0200

    29.10

--- js/scrypt.js
+++ js/scrypt.js
@@ -23,7 +23,7 @@
     refs.openModalBtn.addEventListener("click", toggleModal);
     refs.closeModalBtn.addEventListener("click", toggleModal);
   
-    function toggleModal() {
+  function toggleModal() {
       refs.modal.classList.toggle("is-hidden");
     }
   })();
